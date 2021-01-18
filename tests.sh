90341e12ba5fa24e4a310381ea2d7a1d9af54f2c
Ruslan-Skira <skira.ruslan@gmail.com>
2021-01-18 10:20:22 +0200

    lets do thic coverage

--- tests.sh
+++ tests.sh
@@ -1,4 +1,2 @@
 ENV_ACTIVATE=$(grep ENV_ACTIVATE .env | cut -d '=' -f2)
 source $ENV_ACTIVATE && python  -m pytest --cov=accounts --ignore=data company --cache-clear
-
-#source $ENV_ACTIVATE && python -m pytest --cov=namesgames --ignore=data company --cache-clear
