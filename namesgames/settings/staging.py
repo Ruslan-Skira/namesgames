610d04a8ce3448a2c87fba587df908a603f67fa8
Ruslan-Skira <skira.ruslan@gmail.com>
2021-08-03 16:57:25 +0300

    WHOIS-26: Containerize web app: ~ editing settings files

--- namesgames/settings/staging.py
+++ namesgames/settings/staging.py
@@ -2,17 +2,6 @@ from namesgames.settings.base import *  # NOQA
 
 DEBUG = True
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "namesgames",
-        "USER": "namesgames_admin",
-        "PASSWORD": "1q2w3e4r5t6y",
-        "HOST": "namesgames.c0a07epeefme.us-east-2.rds.amazonaws.com",
-        "PORT": 5432,
-    }
-}
-
 CELERY_BROKER_URL = "rabbitmq3"
 
-ALLOWED_HOSTS=[]
+ALLOWED_HOSTS: list = ['*']
